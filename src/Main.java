5e78e5fdedf122840b1fdde148f283607eddcdef
Shamrock0108 <116237003+Shamrock0108@users.noreply.github.com>
2022-12-23 20:31:09 +0100

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Human human1 = new Human();
+        human1.name = "Tom";
+        human1.age = 18;
+        Human human2 = new Human();
+        human2.name = "Jonh";
+        human2.age = 50;
+        System.out.println("My name " + human1.name + " my age " + human1.age);
+        System.out.println("My name " + human2.name + " my age " + human2.age);
+
     }
 }
 class Human{
     String name;
     int age;
+
+    }
     
   
-}
+
